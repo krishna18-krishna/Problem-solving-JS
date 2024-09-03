1517a852e64127efc93e248012825d02ff062eea
krishna18-krishna <dkrish7182006@gmail.com>
2024-09-03 14:00:11 +0530

    Add files via upload

---
+++ he.js
@@ -0,0 +1,24 @@
+let char ='i';
+
+if (char === 'a' || char=== 'A') {
+  console.log('The character is a vowel.');
+}
+else if (char === 'e' || char==='E') {
+  console.log('The character is a vowel.');
+} 
+else if (char === 'i' || char==='I') {
+    console.log('The character is a vowel.');
+}
+else if (char === 'o' || char==='O') {
+    console.log('The character is a vowel.');
+}
+else if (char === 'u' || char==='U') {
+    console.log('The character is a vowel.');
+}
+
+else {
+  console.log('The character is consonant.');
+}
+
+
+
